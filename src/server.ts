43eb6588db259934bcfa9e392ef807a7c79fa878
pewpola <eduardojor77@gmail.com>
2024-10-30 14:04:14 -0300

    using router

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import cors from "cors"
 import express from "express"
+import { router } from "./routes"
 
 const app = express()
 
 app.use(cors())
+app.use(router);
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Servidor iniciado em <http://localhost>:${PORT}/`))
