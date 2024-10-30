8f4b6a85eb4be692ae602603c2afd57fb6fde3d5
pewpola <eduardojor77@gmail.com>
2024-10-30 14:11:44 -0300

    error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import cors from "cors";
 import express from "express";
 import { router } from "./routes";
 import dotenv from "dotenv";
+import { errorHandlerMiddleware } from "./middlewares/error-handler";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
+app.use(errorHandlerMiddleware);
 app.use(cors());
-app.use(router);
+app.use('/api', router);
 
 const PORT = process.env.API_PORT;
 app.listen(PORT, () => console.log(`Servidor iniciado em <http://localhost>:${PORT}/`));
