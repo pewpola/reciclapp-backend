a1c61bde5bad709ae3f346ae98e4ee3c42151630
pewpola <eduardojor77@gmail.com>
2024-11-20 00:49:17 -0300

    fixing route

--- src/routes/CarrinhoRoutes.ts
+++ src/routes/CarrinhoRoutes.ts
@@ -7,6 +7,6 @@ const router = express.Router();
 router.get('/', authMiddleware, CarrinhoController.getCarrinho);
 router.post('/add', authMiddleware, CarrinhoController.addItem);
 router.delete('/:carrinhoId/item/:itemId', CarrinhoController.removeItem);
-router.delete('/:carrinhoId/clear', CarrinhoController.clearCarrinho);
+router.delete('/clear/:carrinhoId', CarrinhoController.clearCarrinho);
 
 export default router;
